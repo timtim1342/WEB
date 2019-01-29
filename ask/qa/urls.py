12d7e6157e8ce319cbffaedc5ba671f45e68aa2b
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-01-29 19:26:08 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,10 +1,3 @@
 from django.conf.urls import url
 from qa.views import test
-urlpatterns = [
-  url(r'.*', test)
-]
-#from . import views
-
-#urlpatterns = [
- #   url(r'.*', views.test),
-#]
+urlpatterns = [ url(r'^', test)]﻿
