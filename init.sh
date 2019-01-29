5172e7756e9a68f885f2bec3c97a0789996d6578
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-01-29 16:07:27 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 # Gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
-gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:app
+gunicorn -c /etc/gunicorn.d/gunicorn.conf.py views:test
