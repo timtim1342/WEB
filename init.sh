950f2740bb5a914cf75896f2312ecc8407453fda
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-01-29 15:46:14 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,3 @@ sudo /etc/init.d/nginx restart
 # Gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
 gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:app
-sudo service gunicorn restart
