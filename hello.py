5af4d7d0ad614ec6e97accda292b86f4a6172f9a
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-01-29 15:44:02 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
-def wsgi_app(environ, start_response):
+def app(environ, start_response):
     status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
+    headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
-    resp = environ['QUERY_STRING'].split('&')
-    resp = [i+'\r\n' for i in resp]
-    return resp
+    body = [bytes(i + '\r\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    return body
