e23e22123cac51d86f781a76b0e899aac50f7726
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-01-29 19:24:23 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+#from django.contrib import admin
 
 urlpatterns = [
 #    url(r'^admin/', admin.site.urls),
     url(r'^$', include('qa.urls')),
     url(r'^login/$', include('qa.urls')),
     url(r'^signup/$', include('qa.urls')),
-    url(r'^question/\d+/$', include('qa.urls')),
+    url(r'^question/(\d+)/$', include('qa.urls')),
     url(r'^ask/$', include('qa.urls')),
     url(r'^popular/$', include('qa.urls')),
     url(r'^new/$', include('qa.urls')),
