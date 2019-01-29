372570686532105e1fb2c92e3df7cc0c421c5ddd
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-01-29 19:21:22 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'ask',
+    'qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
