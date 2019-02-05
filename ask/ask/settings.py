ab762341827a868f89b88f8a83d640c7e9f81548
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-02-05 16:23:34 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'ask',
-    'qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
