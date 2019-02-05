108cb66e92eef67dc8545c6ff1f5fdb9c88cfe39
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-02-05 17:12:34 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,7 @@ sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
 #gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:app
 #sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
+#sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo ln -sf /home/box/web/etc/gunicorn_django_conf.py /etc/gunicorn.d/gunicorn_django_conf.py
+sudo gunicorn -c /etc/gunicorn.d/gunicorn_django_conf.py ask.wsgi:application
