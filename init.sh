0ed8c52f80114ffb1fa94721a99a8d92aec0a719
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-02-05 16:07:19 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#sudo /etc/init.d/nginx restart
 # Gunicorn
 #sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
 #gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.ask.wsgi:application
+#sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
+#sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo /etc/init.d/gunicorn restart
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
