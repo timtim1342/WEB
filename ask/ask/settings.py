f6d0e09ad41469c131b27f646f8234be5b53eecf
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-02-10 19:28:00 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -76,9 +76,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stepic_web',
-        'USER': 'box',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
