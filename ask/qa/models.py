4319d89287145fb4c59d07570affb00d5ad0f37b
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2019-02-10 18:16:58 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+  title = models.CharField(max_lenght=255)
+  text = models.TextField()
+  added_at = models.DateTimeField(auto_now_add=True)
+  rating = models.IntegerField(default=0)
+  author = models.ForeignKey(User, related_name="question_author")
+  likesn = models.ManyToManyField(
+        User, related_name="question_like", blank=True)
+  
+class Answer(models.Model):
+  text = models.TextField()
+  added_at = models.DateTimeField(auto_now_add=True)
+  question = models.ForeignKey(Question)
+  author = models.ForeignKey(User)
